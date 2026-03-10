8613226171e79c2e24955bccd6843505c1353408
yeraylbede-debug <yeraylbede@gmail.com>
2026-03-10 16:57:53 +0100

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
 import express from 'express';
-import fetch from 'node-fetch';
 
 const app = express();
 const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg2YjFhMGVhLTk5NmEtNDc5OS1hYzUxLTc2MzAyNjgzNzJmZSIsImlhdCI6MTc3MzE1NDQyNywic3ViIjoiZGV2ZWxvcGVyLzlhNzdkZDljLWZiYTgtZTY2Zi1mNjJmLWZhMjNjODc4ZmJmNCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMC4wLjAuMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.4AuO2_tM37m5cGrTiCyCvAeaqc_2sRq3jtkdetCQq8gFU3COwlZVkczUjp4zZoXwKDAQT83shwQhw6bnlY97iA';
 
-app.use((req, res, next) => {
+app.use((req: any, res: any, next: any) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
-app.get('/ip', async (req, res) => {
+app.get('/ip', async (req: any, res: any) => {
   const r = await fetch('https://api.ipify.org?format=json');
   const data = await r.json();
   res.json(data);
 });
 
-app.get('/api', async (req, res) => {
+app.get('/api', async (req: any, res: any) => {
   const target = req.query.url as string;
   if (!target) return res.status(400).json({ error: 'Missing url' });
   try {
